c198f28d332c777283fcb6f1a9547d3e0b41140e
Melike Macineiras <mel.sensivas@gmail.com>
2016-04-30 11:01:26 -0400

    shopping cart round 1

--- week1/ShoppingChart.rb
+++ week1/ShoppingChart.rb
@@ -1,5 +1,4 @@
 class ShoppingCart
-  
   def initialize
     @items = []
   end
@@ -10,11 +9,10 @@ class ShoppingCart
 
   def checkout
   	total = 0
-     @items.each do | single_item |
+    @items.each do | single_item |
       total += single_item.price
     end 
-
-    puts "Your price for today is #{total}"
+    total
   end
 end
 
@@ -30,25 +28,40 @@ end
 
 class Houseware < Item
   def price
-      
+      if @price > 100
+        @price *0.95
+      else
+        @price
+      end
   end
 end
 
-# class Fruit < Item
-#   def price
-#       #Something special may go here too...
-#   end
-# end
 
-# banana = Fruit.new("Banana", 10)
+class Fruit < Item
+  def price
+    today = Time.now
+    if today.saturday? || today.sunday?
+      @price * 0.90
+    else
+      @price
+    end
+  end
+  
+end
 
 my_list = ShoppingCart.new
 
-banana = Item.new "Banana", 10
+banana = Fruit.new "Banana", 10
+Vacuum = Houseware.new "Vacuum", 150
+
 Rice = Item.new "Rice", 1
 Orange_juice = Item.new "Orange_juice", 10
 
 my_list.add_items(banana)
 my_list.add_items(Rice)
+my_list.add_items(banana)
 my_list.add_items(Orange_juice)
-my_list.checkout
+my_list.add_items(Vacuum)
+my_list.add_items(Vacuum)
+
+puts "Your total price today is #{my_list.checkout}. Have a nice day!"
