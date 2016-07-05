3da638278417ca3fc418e7a04812873d8f19a67b
Melike Macineiras <mel.sensivas@gmail.com>
2016-07-05 19:50:48 -0400

    added UPDATE to time_entries

--- timetracking/app/controllers/time_entries_controller.rb
+++ timetracking/app/controllers/time_entries_controller.rb
@@ -1,6 +1,62 @@
 class TimeEntriesController < ApplicationController
+	def update
+		#input project_id time_entry_id
+		@project = Project.find(params[:project_id])
+		@time_entry = @project.time_entries.find(params[:id])
+		time_entry_params = params.require(:time_entry).permit(:hours, :minutes, :date, :comment)
+
+		if @time_entry.update(time_entry_params)
+			# redirect_to action: index, controller: time_entry, project_id: @project_id
+			# OR you can say..
+			redirect_to project_time_entries_path(@project)
+		else
+			render 'edit'
+		end
+		
+	end
+
+	def edit
+		@project = Project.find(params[:project_id])
+		@time_entry = @project.time_entries.find(params[:id])	
+	end
+
 	def index
 		@project = Project.find(params[:project_id])
 		@time_entries = @project.time_entries
 	end
+	def new
+		#find the project
+		@project = Project.find(params[:project_id])
+		#store in a new instans variable
+		@time_entry = @project.time_entries.create
+		
+	end
+
+	def create
+		#input: params[:project_id] and params[:time_entry]
+		@project = Project.find(params[:project_id])
+		time_entry_params = params.require(:time_entry).permit(:hours, :minutes, :date)
+		# time_entry_params == { hours: 1, minutes: 30, date: somedate}
+
+		@time_entry = @project.time_entries.new(params[time_entry_params])
+
+		if @time_entry.save
+			# redirect_to action: index, controller: time_entry, project_id: @project_id
+			# OR you can say..
+			redirect_to project_time_entries_path(@project)
+		else
+			render 'new'
+		end
+		
+	end
+
+
+	# def create...IGNORE THIS
+	# 	find the project
+	# 	@project = Project.find(params[:id])
+	# 	initialize a new time entry
+	# 	@time_entry = @project.time_entries.create
+	# 	save the time entry
+	# 	@time_entry.save	
+	# end
 end
--- timetracking/config/routes.rb
+++ timetracking/config/routes.rb
@@ -1,65 +1,21 @@
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
 Rails.application.routes.draw do
 
   get '/' => 'site#home'
   get '/contact' => 'site#contact'
-  get '/projects' => 'projects#index'
-  get '/projects/new' =>'projects#new'
-  post'/projects' => 'projects#create'
-  get '/projects/:id' => 'projects#show'
-  get '/projects/:project_id/time_entries' => 'time_entries#index'
+
+  resources :projects, only: [:index, :show, :new, :create] do
+    resources :time_entries, only: [:index, :new, :create, :edit, :update]
+  end
+
+  # instead we add EDIT on TOP get '/projects/:project_id/time_entries/3/edit' => 'time_entries#edit'
+  # resources actually gives you all and more of the routes routed below..
+  # you want to see it go to Terminal adn RAKE ROUTES
+  # get '/projects' => 'projects#index'
+  # get '/projects/new' =>'projects#new'
+  # post'/projects' => 'projects#create'
+  # get '/projects/:id' => 'projects#show'
+  # get '/projects/:project_id/time_entries' => 'time_entries#index'
+  # get '/projects/:project_id/time_entries/new' => 'time_entries#new'
 
 end
